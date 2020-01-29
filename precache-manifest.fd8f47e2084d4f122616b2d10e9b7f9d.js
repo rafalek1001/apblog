e592ac5adb5d7d832a96c1ffc6f90f70d9f7b0ec
Your Name <you@example.com>
2020-01-29 19:11:54 +0100

    Updates

---
+++ precache-manifest.fd8f47e2084d4f122616b2d10e9b7f9d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdb0bf5080b7e6cc293b9ccc6a205b1",
+    "url": "/apblog/index.html"
+  },
+  {
+    "revision": "dafcd511f479312e20aa",
+    "url": "/apblog/static/css/main.39a44d4c.chunk.css"
+  },
+  {
+    "revision": "7b0db30987d6e762853c",
+    "url": "/apblog/static/js/2.ea092ce4.chunk.js"
+  },
+  {
+    "revision": "f5ca32966347038a998bbf09dda684c7",
+    "url": "/apblog/static/js/2.ea092ce4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dafcd511f479312e20aa",
+    "url": "/apblog/static/js/main.577ab78c.chunk.js"
+  },
+  {
+    "revision": "97c0cb5d345f80010048",
+    "url": "/apblog/static/js/runtime-main.43f2ecfa.js"
+  },
+  {
+    "revision": "614ad0afaf1ea9a9ea8db3825a8b5512",
+    "url": "/apblog/static/media/contact1.614ad0af.JPG"
+  },
+  {
+    "revision": "61eac64131e760a38effc7284052c24f",
+    "url": "/apblog/static/media/header1.61eac641.JPG"
+  },
+  {
+    "revision": "b50619ad2e5f4b1082cca33d62ec90d8",
+    "url": "/apblog/static/media/header2.b50619ad.JPG"
+  },
+  {
+    "revision": "e2c470919ce691d87ec982ab25c26f0c",
+    "url": "/apblog/static/media/header3.e2c47091.JPG"
+  },
+  {
+    "revision": "afc33269f2d1126d1ddd76d77475ba21",
+    "url": "/apblog/static/media/header4.afc33269.JPG"
+  },
+  {
+    "revision": "822d3f9c5f7507e9df65fbc1caa95109",
+    "url": "/apblog/static/media/header5.822d3f9c.JPG"
+  },
+  {
+    "revision": "d596acecf7ee11c3f213e2db0fac8aaf",
+    "url": "/apblog/static/media/header6.d596acec.JPG"
+  },
+  {
+    "revision": "2258ca71e3d374b340357d1d6e064f40",
+    "url": "/apblog/static/media/img1.2258ca71.jpg"
+  },
+  {
+    "revision": "85e0e905c1ede1e66c13edd672f52f36",
+    "url": "/apblog/static/media/img2.85e0e905.JPG"
+  },
+  {
+    "revision": "8c8b8be93390ce3156e9ee8f50b39aa1",
+    "url": "/apblog/static/media/img3.8c8b8be9.JPG"
+  },
+  {
+    "revision": "a1b5714cdb318f76fdeccfdc90022521",
+    "url": "/apblog/static/media/offer1.a1b5714c.jpg"
+  },
+  {
+    "revision": "567d77cbec6a4de04b195c7110442807",
+    "url": "/apblog/static/media/offer2.567d77cb.jpg"
+  },
+  {
+    "revision": "a973c3f90f9ce4d70ac405c73a152896",
+    "url": "/apblog/static/media/offer3.a973c3f9.JPG"
+  },
+  {
+    "revision": "4087b593f68fece23730b3d9e267f739",
+    "url": "/apblog/static/media/offer4.4087b593.JPG"
+  },
+  {
+    "revision": "5fc8a87f254161defa45c88ae6d8afb6",
+    "url": "/apblog/static/media/offer5.5fc8a87f.JPG"
+  },
+  {
+    "revision": "ceb5b144190f6475f09b4d6156cb603a",
+    "url": "/apblog/static/media/offer6.ceb5b144.JPG"
+  }
+]);
